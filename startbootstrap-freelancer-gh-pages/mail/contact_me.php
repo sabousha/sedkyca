14752be22804a27636da0968e2e302f0d54135fc
sabousha <sabousha@uwo.ca>
2018-03-25 12:52:04 -0400

    fixing contact_me part 1

--- startbootstrap-freelancer-gh-pages/mail/contact_me.php
+++ startbootstrap-freelancer-gh-pages/mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -16,7 +15,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 's_aboushamalah@outlook.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'sedkya@outlook.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@sedky.ca\n";
